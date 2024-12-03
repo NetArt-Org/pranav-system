0cdb2cb5baccee869c3fef15b5aa6b11f7a8e7ea
@Kavita2599 <kavita99@netart.io>
2024-12-03 21:31:16 +0530

    contactFormStyle

--- components/contactForm.js
+++ components/contactForm.js
@@ -2,13 +2,13 @@ import React from 'react'
 function contactForm() {
     // const [state, handleSubmit] = useForm("myzgvndp");
     const InputStyle={
-width:"98%",background:"#F9F9F9",border:"1px solid #7F7F7F",padding:"20px 0 20px 14px"
+width:"98%",background:"#F9F9F9",border:"1px solid #7F7F7F",padding:"14px 0 14px 8px"
     }
     return (
         <>
            <form style={{display:"flex",flexDirection:"column",gap:"24px",flexWrap:"wrap",flex:"1"}}>
                 <input style={InputStyle} type='text' name='name' placeholder='Full Name' required></input>
-                <div style={{display:"flex",justifyContent:"space-between"}}>
+                <div className='form-div'  style={{display:"flex",justifyContent:"space-between"}}>
                 <input style={{...InputStyle,width:"46%"}} className='w-half' type='email' name='address' placeholder='Email Address' required></input>
                 <input style={{...InputStyle,width:"46%"}} className='w-half' type='tel' name='number' placeholder='Phone Number' required></input>
                 </div>
